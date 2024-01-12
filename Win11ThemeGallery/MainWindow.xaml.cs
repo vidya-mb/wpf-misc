31ef6d1571a5d547fe7d94da03411f84153e2fd2
dipeshmsft <dipeshkumar@microsoft.com>
2024-01-12 21:39:21 +0530

    Added Button page and MainWindow window

--- Win11ThemeGallery/MainWindow.xaml.cs
+++ Win11ThemeGallery/MainWindow.xaml.cs
@@ -9,6 +9,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Windows.Appearance;
+
 namespace Win11ThemeGallery;
 
 /// <summary>
@@ -18,6 +20,36 @@ public partial class MainWindow : Window
 {
     public MainWindow()
     {
+        SystemThemeWatcher.Watch(this);
+        ViewModel = new MainWindowViewModel();
+        DataContext = this;
         InitializeComponent();
+    }
+
+    public MainWindowViewModel ViewModel { get; }
+
+    private void ControlsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (ControlsList.SelectedItem is string selectedItem)
+        {
+            string? controlName = selectedItem;
+            SetPage(controlName);
+        }
+    }
+
+    private void SetPage(string? controlName)
+    {
+        if (!string.IsNullOrEmpty(controlName))
+        {
+            switch(controlName)
+            {
+                case "Button":
+                    // RootContentDialog.Content = new ButtonPage().Content;
+                    RootContentFrame.Source = new Uri(".\\Views\\ButtonPage.xaml", UriKind.RelativeOrAbsolute);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
